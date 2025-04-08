f91ed48bbe590e5e4e7286accd7752b1d15fa7e8
shahanasherin-p <shahanasherinp062@gmail.com>
2025-04-08 15:56:05 +0530

    v4

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatappserver-k9gm.onrender.com/api" : "/api",
+  baseURL: "https://chatappserver-k9gm.onrender.com/api",
+
   withCredentials: true,
 });
